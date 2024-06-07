5865d19c439eb0fb4907aa3aabf07634f1da7281
jud-as <guilhermeferreira082@gmail.com>
2024-06-07 15:00:57 -0300

    Projeto finalizado.

--- src/Hospital.java
+++ src/Hospital.java
@@ -26,19 +26,19 @@ public class Hospital {
         int opc = input.nextInt();
         switch(opc) {
             case 1:
-                triagem(cadastrarPaciente());
+                triagem(null);
                 break;
             case 2:
-                //excluirPaciente();
+                excluirPaciente();
                 break;
             case 3:
-                //atualizarPaciente();
+                atualizarPaciente();
                 break;
             case 4:
-                //cadastrarMedico();
+                cadastrarMedico();
                 break;
             case 5:
-                //listar();
+                listar();
                 break;
             case 6:
                 System.out.println("Encerrando programa.");
@@ -62,7 +62,6 @@ public class Hospital {
         System.out.println("\nNível de dor: \n1 - Sem dor\n2 - Dor leve\n3 - Dor moderada\n4 - Dor intensa\n5 - Dor insuportável\n");
         int nivelDor = input.nextInt();
         Paciente paciente = new Paciente(nome, cpf, idade, nivelDor, "", 0, 0);
-        arrayPacientes.add(paciente);
         System.out.println("Paciente cadastrado com sucesso.");
         return paciente;
     }
@@ -73,34 +72,41 @@ public class Hospital {
         System.out.println("\nCPF: ");
         String cpf = input.next();
         System.out.println("\nCRM: ");
-        String crm = input.next();
+        int crm = input.nextInt();
         Medico medico = new Medico(nome, cpf, crm);
         arrayMedicos.add(medico);
         System.out.println("Médico cadastrado com sucesso.");
+        operacoes();
     }
 
     public static void triagem(Paciente paciente) {
-        paciente = cadastrarPaciente();
-        if (paciente.getNivelDor() <= 3) {
-            try {
-                System.out.println("\nPaciente encaminhado para a medição.");
-                System.out.println("\nPressão: ");
-                String pressao = input.next();
-                System.out.println("\nAltura: ");
-                float altura = input.nextFloat();
-                System.out.println("\nPeso: ");
-                float peso = input.nextFloat();
-                paciente.setPressao(pressao);
-                paciente.setAltura(altura);
-                paciente.setPeso(peso);
-            } catch (Exception e) {
-                System.out.println("Erro ao cadastrar paciente.");
-            } finally {
+        if (!arrayMedicos.isEmpty()){
+            paciente = cadastrarPaciente();
+            if (paciente.getNivelDor() <= 3) {
+                try {
+                    System.out.println("\nPaciente encaminhado para a medição.");
+                    System.out.println("\nPressão: ");
+                    String pressao = input.next();
+                    System.out.println("\nAltura: ");
+                    float altura = input.nextFloat();
+                    System.out.println("\nPeso: ");
+                    float peso = input.nextFloat();
+                    paciente.setPressao(pressao);
+                    paciente.setAltura(altura);
+                    paciente.setPeso(peso);
+                } catch (Exception e) {
+                    System.out.println("Erro ao cadastrar paciente.");
+                } finally {
+                    consulta(paciente, getRandomMedico());
+                }
+            } else {
                 consulta(paciente, getRandomMedico());
             }
         } else {
-            consulta(paciente, getRandomMedico());
+            System.out.println("Não há médicos disponíveis.");
+            System.out.println("Cadastre um médico para continuar.");
         }
+        arrayPacientes.add(paciente);
         operacoes();
     }
     public static void consulta(Paciente paciente, Medico medico) {
@@ -120,11 +126,45 @@ public class Hospital {
         }
     }
 
+    public static void atualizarPaciente() {
+        System.out.println("\nDigite o CPF do paciente que deseja atualizar: ");
+        String cpf = input.next();
+        for (Paciente paciente : arrayPacientes) {
+            if (paciente.getCpf().equals(cpf)) {
+                System.out.println("\nDigite o novo nome: ");
+                String nome = input.next();
+                System.out.println("\nDigite a nova idade: ");
+                int idade = input.nextInt();
+                System.out.println("\nDigite o novo nível de dor: ");
+                int nivelDor = input.nextInt();
+                paciente.setNome(nome);
+                paciente.setIdade(idade);
+                paciente.setNivelDor(nivelDor);
+                System.out.println("\nPaciente atualizado com sucesso.");
+                operacoes();
+            } else{
+                System.out.println("Paciente não encontrado.");
+            }
+        }
+        operacoes();
+    }
+
     public static Medico getRandomMedico() {
         return arrayMedicos.get((int) (Math.random() * arrayMedicos.size()));
     }
 
+    public static void listar(){
+        System.out.println("Pacientes: ");
+        for (Paciente paciente : arrayPacientes) {
+            System.out.println("Nome: " + paciente.getNome() + " | CPF: " + paciente.getCpf() + " | Idade: " + paciente.getIdade() + " | Nível de dor: " + paciente.getNivelDor());
+        }
+        System.out.println("Médicos: ");
+        for (Medico medico : arrayMedicos) {
+            System.out.println("Nome: " + medico.getNome() + " | CPF: " + medico.getCpf() + " | CRM: " + medico.getCrm());
+        }
 
+        operacoes();
+    }
 
 
 }
